47f4e37bc8f6410a378c64e1dea3d8cc499a89fd
Karl Hedayati (Étudiant) <202396735@mfl-c0240-26.ahuntsic.ca.intra>
2024-06-13 19:28:52 -0400

    page accueil

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -20,7 +20,15 @@
       
         <div class="ion-text-center">
           <ion-img :src="img"></ion-img>
-          <p>{{ meal }}</p>
+          <p class="meal-text">{{ meal }}</p>
+          <p>Origine: {{ area }}</p>
+          <p>Category: {{ categorie }}</p>
+          <p class="meal-text">Ingrédients</p>
+          <ion-list>
+            <ion-item v-for="ingredient in ingredients" :key="ingredient.id">
+              <ion-label>{{ ingredient.name }}</ion-label>
+            </ion-item>
+          </ion-list>
           <p>Page d'accueil</p>
           <ion-button expand="block" color="medium" href="/pageCinq">
             Page Cinq</ion-button>
@@ -45,12 +53,18 @@ import { IonContent,
          IonButton,
          IonButtons,
          IonMenuButton,
-         IonImg } from '@ionic/vue';
-import { ref, onMounted } from 'vue';
+         IonImg,
+         IonItem,
+         IonList,
+         IonLabel } from '@ionic/vue';
+import { ref, onMounted, reactive } from 'vue';
 import { loadingController } from '@ionic/vue';
 
 const meal = ref("");
 const img = ref("");
+const area = ref("");
+const categorie = ref("");
+const ingredients = reactive([{}]);
 
 
   onMounted(async () => {
@@ -58,6 +72,7 @@ const img = ref("");
     await loading.present();
     //let url = 'https://StephaneDGill.github.io/disque.json';
     let url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    let ingredientId = 1;
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -67,6 +82,19 @@ const img = ref("");
         //meal.value = data.titre_album;
         meal.value = data.meals[0].strMeal;
         img.value = data.meals[0].strMealThumb;
+        area.value = data.meals[0].strArea;
+        categorie.value = data.meals[0].strCategory;
+        for (let i = 1; i <= 20; ++i) {
+          if (data.meals[0]['strIngredient' + i] !== "" && data.meals[0]['strIngredient' + i] !== null) {
+            ingredients.push({ id: i, name: `${data.meals[0]['strIngredient' + i]}` })
+          }
+        }
+        //console.log(ingredients);
+        let str = "data.meals[0].strIngredient" + "2";
+        console.log(ingredients);
+        //console.log(`${data.meals[0].strIngredient}`);
+        //console.log(`${data.meals[0].strIngredient1}`);
+        //console.log(`${str}`);
         loading.dismiss();
         }); 
   });
@@ -74,6 +102,12 @@ const img = ref("");
 </script>
 
 <style scoped>
+
+.meal-text {
+  font-size: 20px;
+  font-weight: bold;
+}
+
 #container {
   text-align: center;
   
