f02ec4dd008f6f54c82d59318eed0a18d49dac65
Karl Hedayati (Étudiant) <202396735@mfl-c0240-26.ahuntsic.ca.intra>
2024-06-16 19:54:52 -0400

    ElementRecherche.vue ok

--- src/components/ElementRecherche.vue
+++ src/components/ElementRecherche.vue
@@ -41,10 +41,14 @@
   import RecetteItem from '@/components/RecetteItem.vue';
 
 
-  const props = defineProps<{ id: string | string[] }>();
+  const props = defineProps<{ id: string }>();
   console.log(`id = ${props.id}`);
   const recettesArray = ref<Recette[]>([]);
   let url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken';
+  const categoriesArray :string[] = [];
+
+  const categories: string[] = ["Beef", "Chicken", "Dessert", "Lamb", "Miscellaneous", "Pasta", "Pork", "Seafood", "Side", "Starter", "Vegan", "Vegetarian", "Breakfast", "Goat"];
+
 
 
   if (props.id !== null) {
@@ -52,7 +56,17 @@
     const loading = await loadingController.create({ message: 'Attendre SVP...', });
     await loading.present();
     url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${props.id}`;
-    
+
+
+    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        data.categories.forEach((category: string) => {
+            categoriesArray.push(category);
+        })
+    });
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
