3a66742f9787a6e3d1445f74a8d3d40e0327cf1a
Karl Hedayati (Étudiant) <202396735@mfl-c0240-26.ahuntsic.ca.intra>
2024-06-16 19:34:51 -0400

    modified ElementRecherche.vue

--- src/components/ElementRecherche.vue
+++ src/components/ElementRecherche.vue
@@ -33,7 +33,6 @@
   <script setup lang="ts">
   import { IonPage, IonFooter, IonToolbar, IonTitle, IonContent, IonCard, IonItem, IonList, IonHeader, IonButtons, IonMenuButton, IonSkeletonText } from '@ionic/vue'
   import MonHeader from '@/components/MonHeader.vue'
-//   import ComposantLivre from '@/components/ComposantLivre.vue'
   import ComposantRecette from '@/components/ComposantRecette.vue';
   import RecherchePage from '@/views/RecherchePage.vue';
   import { Recette } from '../types';
@@ -52,10 +51,7 @@
     onMounted(async () => {
     const loading = await loadingController.create({ message: 'Attendre SVP...', });
     await loading.present();
-    //const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken';
     url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${props.id}`;
-    
-    const ingredientsStringList = ["strIngredient1", "strIngredient2", "strIngredient3", "strIngredient4", "strIngredient5", "strIngredient6", "strIngredient7", ]
     
     fetch(url)
         .then(response => response.json())
@@ -65,8 +61,6 @@
           //console.log(data['meals'][0]);
           //console.log(`data['meals'][0].length = ${data['meals'][0].value.length}`);
           
-          
-
           data['meals'].forEach((meal: Recette) => {
               //console.log(meal);
               //console.log(meal.strMeal);
@@ -90,31 +84,7 @@
               recettesArray.value.push(recetteValue);
               //console.log(recettesArray.value[0].strMeal);
           })
-          recettesArray.value.forEach((recette) => {
-              //console.log(`strMeal: ${recette.strMeal}`);
-          });
-          
-          /*
-          console.log(`data['meals'][0].idMeal = ${data['meals'][0].idMeal}`);
-          const ingredientsArray: { mesure: string, name: string }[] = [];
-          
-          for (let i = 1; i <= 20; ++i) {
-            if (data.meals[0]['strIngredient' + i] !== "" && data.meals[0]['strIngredient' + i] !== null) {
-              ingredientsArray.push({ mesure: data.meals[0]['strMeasure' + i], name: `${data.meals[0]['strIngredient' + i]}` })
-            }
-          }
-          const recetteValue: Recette = {
-            nomRecette: data.meals[0].strMeal,
-            img: data.meals[0].strMealThumb,
-            area: data.meals[0].strArea,
-            categorie: data.meals[0].strCategory,
-            ingredients: ingredientsArray,
-            instructions: data.meals[0].strInstructions,
-          };
-          console.log(recetteValue.ingredients);
-          */
           loading.dismiss();
-          //recette.value = recetteValue;
         });
   });
   }
